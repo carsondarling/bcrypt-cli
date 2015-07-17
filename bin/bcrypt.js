343bdd25e8b2e9a585469441d734e5ec0810416a
Carson Darling <carsondarling@gmail.com>
2015-07-17 12:27:54 -0400

    initial commit

---
+++ bin/bcrypt.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+var package = require('../package.json');
+var bcrypt = require('bcrypt');
+var ArgumentParser = require('argparse').ArgumentParser;
+
+var parser = new ArgumentParser({
+  version: package.version,
+  addHelp: true,
+  description: 'Bcrypt ALL the things!'
+});
+
+parser.addArgument(['-s', '--salt'], {
+  help: 'Pre-generated salt'
+});
+
+parser.addArgument(['-V', '--verbose'], {
+  action: 'storeTrue',
+  help: 'Enable verbose output'
+});
+
+parser.addArgument(['-r', '--rounds'], {
+  help: 'Number of rounds to use (default 10)',
+  defaultValue: 10,
+  type: 'int'
+});
+
+parser.addArgument(['rawText' ], {
+  help: 'The data to encrypt'
+});
+
+var args = parser.parseArgs();
+
+if (args.verbose) console.error('Generating salt with %s rounds.', args.rounds);
+var salt = bcrypt.genSaltSync(args.rounds);
+
+if (args.verbose) console.error('Generating hash.');
+var hash = bcrypt.hashSync('B4c0/\/', salt);
+
+if (args.verbose) process.stderr.write('Output: ');
+console.log(hash);
