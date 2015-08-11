60bc591e80ada4ccef6578e6e213678186c29e04
Carson Darling <carsondarling@gmail.com>
2015-08-11 19:27:40 -0400

    added ability to read from stdin, and do "password mode"

--- bin/bcrypt.js
+++ bin/bcrypt.js
@@ -24,18 +24,107 @@ parser.addArgument(['-r', '--rounds'], {
   type: 'int'
 });
 
-parser.addArgument(['rawText' ], {
+parser.addArgument(['rawText'], {
+  nargs: '?',
   help: 'The data to encrypt'
 });
 
 var args = parser.parseArgs();
 
-if (args.verbose) console.error('Generating salt with %s rounds.', args.rounds);
-var salt = bcrypt.genSaltSync(args.rounds);
+// Handle various input options
+if (args.rawText) {
+  processInput(null, args.rawText);
+} else if (process.stdin.isTTY) {
+  readTTY(processInput);
+} else {
+  readFile(processInput);
+}
 
-if (args.verbose) console.error('Generating hash.');
-var hash = bcrypt.hashSync(args.rawText, salt);
 
-if (args.verbose) process.stderr.write('Output: ');
-process.stdout.write(hash);
-process.stderr.write('\n');
+/**
+ * Helpers
+ * =============================================================================
+ */
+
+/**
+ * Take input, run it through bcrypt with the right parameters, and output
+ * results.
+ */
+function processInput(err, input) {
+  if (err) process.exit(1);
+
+  if (args.verbose) console.error('Generating salt with %s rounds.', args.rounds);
+  var salt = bcrypt.genSaltSync(args.rounds);
+
+  if (args.verbose) console.error('Generating hash.');
+  var hash = bcrypt.hashSync(input, salt);
+
+  if (args.verbose) process.stderr.write('Output: ');
+  process.stdout.write(hash);
+  process.stderr.write('\n');
+}
+
+
+/**
+ * Read stdin as a pipe
+ */
+function readFile(callback) {
+  stdInput = '';
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('data', function(chunk) {
+    stdInput += chunk;
+  });
+
+  process.stdin.on('end', function() {
+    callback(null, stdInput);
+  });
+
+  process.stdin.on('error', callback);
+
+  process.stdin.resume();
+}
+
+
+/**
+ * Read stdin as a TTY
+ */
+function readTTY(callback) {
+  // Display prompt
+  process.stderr.write('Raw text: ');
+
+  var stdInput = '';
+  process.stdin.setEncoding('utf8');
+  process.stdin.setRawMode(true);
+
+  process.stdin.on('error', callback);
+
+  process.stdin.on('data', function(chunk) {
+    switch (chunk) {
+
+      // They've finished typing their password
+      case '\n':
+      case '\r':
+      case '\u0004':
+        process.stderr.write('\n');
+        process.stdin.setRawMode(false);
+        process.stdin.pause();
+        callback(null, stdInput);
+        break;
+
+      // Ctrl-C
+      case '\u0003':
+        process.stderr.write('\n');
+        process.exit(1);
+        break;
+
+      // More passsword characters
+      default:
+        process.stderr.write('*');
+        stdInput += chunk;
+        break;
+    }
+  });
+
+  process.stdin.resume();
+}
